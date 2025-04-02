29a0420b320f20cea35d22596d23d8ce0c446c17
medaaouni <mohamedaaouni2006@gmail.com>
2025-04-02 12:21:50 +0000

    test: add the test for pokemon color directive

--- src/app/core/directives/type-color.directive.spec.ts
+++ src/app/core/directives/type-color.directive.spec.ts
@@ -1,8 +1,34 @@
-import { TypeColorDirective } from './type-color.directive';
+import {TypeColorDirective} from './type-color.directive';
+import {Component} from '@angular/core';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
+
+@Component({
+  template: `
+    <div [appTypeColor]="'fire'"></div>
+    <svg [appTypeColor]="'fire'" class="test-svg">
+      <path d="M10 10"></path>
+    </svg>`,
+  imports: [
+    TypeColorDirective
+  ]
+})
+class TestComponent {
+}
 
 describe('TypeColorDirective', () => {
-  it('should create an instance', () => {
-    const directive = new TypeColorDirective();
-    expect(directive).toBeTruthy();
+
+  let fixture: ComponentFixture<TestComponent>;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({})
+
+    fixture = TestBed.createComponent(TestComponent);
+    fixture.detectChanges();
+  })
+
+
+  it('should set the correct background color for fire type', () => {
+    const div: HTMLElement = fixture.nativeElement.querySelector('div');
+    expect(div.style.backgroundColor).toEqual('rgb(238, 128, 59)');
   });
 });
